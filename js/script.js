d6df4057567bb0ab525b3896911c683869e941a6
tnjensen <thomasnorupjensen@gmail.com>
2022-09-21 16:19:24 +0200

    Refactored site to single page, added headless cms

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 const links = document.querySelectorAll('nav a');
 const menuBtn = document.querySelector('.menu-btn');
+const projects = document.querySelector('.projects');
+const url = 'https://noroff.tnjensen.com/portfolio/wp-json/wp/v2/posts?_embed&filter[orderby]=date&order=asc';
+const corsEnabledUrl ="https://noroffcors.herokuapp.com/";
+const loader = document.querySelector('.loader');
+let postsPerPage = 0;
+let postResult = [];
 
 for(let i = 0; i < links.length; i++){
     if(links[i] == document.URL){
@@ -10,3 +16,43 @@ for(let i = 0; i < links.length; i++){
 menuBtn.addEventListener('click', function(){
     menuBtn.classList.toggle('visible');
 })
+
+window.addEventListener('mouseup', function(event){
+    if(!event.target.closest('.menu-btn')){
+        menuBtn.classList.remove('visible');
+    }
+})
+
+async function getProjects(){
+    try{
+        let response = await fetch(url);
+        let results = await response.json();
+        loader.innerHTML = "";
+        loader.classList.remove('loading-indicator');
+        createHTML(results);
+    
+    }
+    catch(error){
+        if(projects){
+            projects.innerHTML = `Error: ` + error;
+        }     
+    }
+}
+getProjects();
+
+function createHTML(results){ 
+    for(let i = 0; i < results.length; i++){ 
+        if(!results[i]){
+            console.log('No results');
+            break;
+        }else{
+            projects.innerHTML += `<div class="project-item">
+                <h2>${results[i].title.rendered}</h2>
+                <img src="${results[i]._embedded['wp:featuredmedia'][0].source_url}" alt="${results[i]._embedded['wp:featuredmedia'][0].alt_text}" />
+                <p>${results[i].content.rendered}</p>
+                <a href="${results[i].meta._links_to}">Visit site</a>
+                </div>`; 
+        }
+    }
+    
+}
